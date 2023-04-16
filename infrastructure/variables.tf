b4154360970dc8e788736325808682e81027aa3c
James Croney (Thinbox) <techtruth@gmail.com>
2023-04-16 10:20:18 -0700

    updated api

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,7 +15,7 @@ variable "hosted_zone" {
 
 variable "github_pat" {
   description = "Github Personal Access Token for github authentication"
-  default     = "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"
+  default     = "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"
 }
 variable "github_repo" {
   description = "Github repo to publish deployment secrets from provisioning"
